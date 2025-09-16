9a485cdedfc388ee928e3bb89ba7c7c51029ad3d
Daniela Escobar <escobarhenaodaniela@gmail.com>
2025-09-16 08:45:53 -0500

    agregar cambios menores al formulario de log in y agregarle icono

--- asistencia/script.js
+++ asistencia/script.js
@@ -5,13 +5,13 @@ const password = document.getElementById("password");
 
 // Identificar actividad en el botón
 loginBtn.addEventListener("click", () => {
-  // Validar si hay algún campo vacío
+ 
   if (username.value.trim() === "" || password.value.trim() === "") {
     alert("Ingreso de datos no válido, por favor, completa todos los campos.");
     return;
   }
 
-  // Confirmación visual al apretar el botón de Log In
+  // Confirmación visual
   loginBtn.innerText = "Guardado";
   loginBtn.classList.remove("btn-primary");
   loginBtn.classList.add("btn-success");
